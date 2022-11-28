90300063c4fa38b6c9ff82b8438ad53cd612d497
Dat Huynh <ahythel@gmail.com>
2022-11-28 13:58:51 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// Taken from leetcode
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode* prev;
+    ListNode() : val(0), next(nullptr), prev(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr), prev(nullptr) {}
+    ListNode(int x, ListNode* next, ListNode* prev) : val(x), next(next), prev(prev) {}
+};
+
+
+int main() {
+
+
+
+
+
+	system("pause");
+}
